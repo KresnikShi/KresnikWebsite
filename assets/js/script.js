8ffd006fe15075b4a7fa1fbf8f516136d90c272a
KresnikShi <kresnik.jun@gmail.com>
2018-09-18 20:22:14 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
+	// hide the audios
 	$(".allaudios").hide();
 
+	// click the titles
     $("h3").click(function() {
 		$(this).next("div.allaudios").slideToggle();
 	});
