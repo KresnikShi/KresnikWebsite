e64832ef2c43589f69833191737e7ec650067ee8
KresnikShi <kresnik.jun@gmail.com>
2018-09-18 19:22:17 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 
 	$(".allaudios").hide();
 
+	$("h2").click(function() {
+		$(".title_handling").slideToggle();
+	});
+
     $("h3").click(function() {
 		$(".allaudios").slideToggle();
 	});
