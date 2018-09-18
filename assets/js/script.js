e2ca60547e605ccc66689754744e35debf166598
KresnikShi <kresnik.jun@gmail.com>
2018-09-18 20:25:03 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
+	// javascript
 	// hide the audios
 	$(".allaudios").hide();
 
 	// click the titles
     $("h3").click(function() {
-		$(this).next("div.allaudios").slideToggle();
+		$(this).next(".allaudios").slideToggle();
 	});
 });
