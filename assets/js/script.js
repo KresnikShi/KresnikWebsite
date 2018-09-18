ac1a8e746e1a66a208400cca278a175d48a5bf40
KresnikShi <kresnik.jun@gmail.com>
2018-09-18 19:11:07 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,8 @@
-// jQuery(document).ready(function () {
-
-// 	// Initially hide the text
-// 	jQuery.(".allaudios").hide();
-
-// 	// Handling the title
-// 	jQuery.(".title_handling h4").click(function() {
-// 		jQuery(this).next(".audios").slideToggle();
-// 	});
-// });
-
 $(document).ready(function(){
 
 	$(".allaudios").hide();
 
-    $(".title_handling h3").click(function() {
+    $("h4").click(function() {
 		jQuery(this).next(".allaudios").slideToggle();
 	});
 });
